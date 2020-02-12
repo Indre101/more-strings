4fe26a6580e974b0d553eb8a78491ef10586e1ec
indre101 <zygaityte.indre@gmail.com>
2020-02-12 13:45:28 +0100

    check the file extension

--- script.js
+++ script.js
@@ -10,5 +10,30 @@ document.querySelector("input[type=submit]").onclick = function () {
 
 const capitalise = (element) => element.charAt(0).toUpperCase() + element.substring(1, element.length);
 const findFirstName = (element) => element.substring(0, element.indexOf(" "));
+const getLengthOfName = element => element.length;
 
-console.log(findFirstName("Sara Lnk"));
+const getHowManySpaces = (element) => {
+  let firstGap = element.indexOf(" ");
+  let index = 0;
+  while (firstGap !== -1) {
+    index++
+    firstGap = element.indexOf(" ", (firstGap + index))
+  }
+  return index
+}
+
+function getMiddleName(element) {
+  const names = element.split(" ");
+  let middleName
+  if (names.length >= 2 && names[2].length >= 2) {
+    middleName = names[1];
+  } else {
+    middleName = "Middle name not found"
+  }
+  return middleName
+}
+
+function checkFileExtension(element) {
+  return element.endsWith(".jpg") || element.endsWith(".png")
+}
+console.log(checkFileExtension("Sara.pngk"));
