7fa4acb6d5faf0bc8fc81931724b4fc1f3ac6f79
indre101 <zygaityte.indre@gmail.com>
2020-02-12 14:41:32 +0100

    finished functionality

--- script.js
+++ script.js
@@ -1,26 +1,14 @@
 const userInput = document.querySelector("input[type=text]");
-
+const option = document.querySelector("select");
+const options = document.querySelectorAll("option");
+const userOutput = document.querySelector(".output");
 
 const inputValue = () => userInput.value;
 
-document.querySelector("input[type=submit]").onclick = function () {
-  console.log(inputValue());
-  event.preventDefault();
-}
-
 const capitalise = (element) => element.charAt(0).toUpperCase() + element.substring(1, element.length);
 const findFirstName = (element) => element.substring(0, element.indexOf(" "));
-const getLengthOfName = element => element.length;
+const getLengthOfName = element => findFirstName(element).length;
 
-const getHowManySpaces = (element) => {
-  let firstGap = element.indexOf(" ");
-  let index = 0;
-  while (firstGap !== -1) {
-    index++
-    firstGap = element.indexOf(" ", (firstGap + index))
-  }
-  return index
-}
 
 function getMiddleName(element) {
   const names = element.split(" ");
@@ -44,4 +32,47 @@ function hidePassword(element) {
 
 const capitaliseThird = (element) => element.substring(0, 2) + element[2].toUpperCase() + element.substring(2, element.length);
 
-console.log(capitaliseThird("Sara.pngk"));
+
+function capitaliseAfterGapsHyphen(element) {
+  const arr = element.split(" ");
+  const newSentence = [];
+  arr.forEach(word => {
+    if (word !== " " || word !== "-") {
+      let capitalisedWord = word.charAt(0).toUpperCase() + word.substring(1, word.length);
+      newSentence.push(capitalisedWord);
+    } else {
+      newSentence.push(word);
+    }
+  });
+
+  const capitalisedSentence = newSentence.join(" ");
+  return capitalisedSentence
+}
+
+options.forEach(op => op.selected = false);
+
+
+option.addEventListener("change", () => {
+  let element = inputValue();
+  let result;
+  if (option[0].selected == true) {
+    result = element
+  } else if (option[1].selected == true) {
+    result = capitalise(element)
+  } else if (option[2].selected == true) {
+    result = findFirstName(element)
+  } else if (option[3].selected == true) {
+    result = getLengthOfName(element)
+  } else if (option[4].selected == true) {
+    result = getMiddleName(element)
+  } else if (option[5].selected == true) {
+    result = checkFileExtension(element)
+  } else if (option[6].selected == true) {
+    result = hidePassword(element)
+  } else if (option[7].selected == true) {
+    result = capitaliseThird(element)
+  } else if (option[8].selected == true) {
+    result = capitaliseAfterGapsHyphen(element)
+  }
+  userOutput.textContent = result;
+})
